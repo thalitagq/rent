0a16670b157dea4175b9672c23503c6ea01e4759
thalita <thalita@Ubuntu.myguest.virtualbox.org>
2023-04-24 18:45:05 -0300

    adciona seed de admin

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -7,6 +7,7 @@ import {
   PrimaryColumn,
 } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+
 import { Category } from "./Category";
 
 @Entity("cars")
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,33 +1,37 @@
+/* eslint-disable import/order */
 import "reflect-metadata";
-import express , { NextFunction, Request, Response } from "express"
-import "express-async-errors"
-import { router } from "./routes"
-import swaggerUI from 'swagger-ui-express'
-import "../typeorm"
- 
-import "../../container"
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swaggerUI from "swagger-ui-express";
 
-import swaggerFile from '../../../swagger.json'
+import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm/index";
 
-const app = express()
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
 
-app.use(express.json())
+createConnection();
+const app = express();
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
+app.use(express.json());
 
-app.use(router)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
-app.use(( err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message
-    })
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
   }
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`
-  })
-})
+);
 
-app.listen(3333, () => console.log("Server is running"))
+app.listen(3333, () => console.log("Server is running"));
